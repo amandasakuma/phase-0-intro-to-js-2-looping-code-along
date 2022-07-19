042b07628ed1a60c6b10c4bc4e20e8eb198fca3d
Amanda Sakuma <amandasakuma@gmail.com>
2022-07-19 11:55:04 -0400

    commit tree 632df3eb4a774cb702f0d1c29a74620fcd13ade7

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"]
+
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+    for (let i = 0; i <namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+function countDown() {
+    let startingNumber = 11;
+    while (startingNumber > 0) {
+        console.log(startingNumber -= 1);
+    }
+    return countDown
+}
+// function countDown(number) {
+//     while (number < 0) {
+//         console.log( number); 
+//         number -= 1;
+//     }
+//     console.log( number );
+// }
